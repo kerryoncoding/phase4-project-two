cb0a47c4d1dedb39db1c4107c295a4c880279bfa
Kerry Guarino <kerry.guarino@gmail.com>
2024-03-07 19:41:50 -0500

    connected projects which I will turn into squads

--- client/src/components/Squads.jsx
+++ client/src/components/Squads.jsx
@@ -1,14 +1,50 @@
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react"
+import Projects from "./Projects"
 
-function Squads(){
+function Squads() {
+   const [projectList, setProjectList] = useState([])
+   const URL = "http://localhost:5173/components/projects"
 
+   useEffect(()=>{
+      fetch(URL)
+      .then(res => res.json())
+      .then(data => setProjectList(data))
+   },[])
+
+   
+   function deleteItem(item){
+      fetch(`${URL}/${item}`, {
+         method: "DELETE",
+      })
+      .then(res => res.json())
+      .then(data=> {
+         let updatedList = projectList.filter((data)=> data.id != item)
+         setProjectList(updatedList)
+      })
+   }
+
+
+   function addProject(newProject){
+      fetch("http://localhost:5173/components/projects", {
+         method: "POST",
+         headers: {
+            "Content-Type":"application/json"
+         },
+         body: JSON.stringify(newProject)
+      })
+      .then(res => res.json())
+      .then(data => {
+         setProjectList([...projectList, data])
+      })
+   }
 
 
    return (
       <div className="about-container">
          <h2>LIST ALL SQADS</h2>
          <div className="card-container">
-         <h2>PROJECTS</h2>
+            <h2>PROJECTS</h2>
+            <Projects projectList={projectList} addProject={addProject} deleteItem={deleteItem} />
          </div>
       </div>     
    )
